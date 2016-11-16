ffca4a397c97079acd348eb523c33da0bf46b390
cts <cts@ctsdeiMac-2.lan>
2016-11-16 18:51:00 +0800

    更改设置窗口无标题

--- app/src/main/java/com/example/cts/ex/MainActivity.java
+++ app/src/main/java/com/example/cts/ex/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cts.ex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
     }
 
